66d7a22f12bc6681fefca81e75c31078041b3fbf
Danny DeBelius <danny.debelius@gmail.com>
2012-02-02 11:01:07 -0500

    increase timeout on disqus

--- js/app.js
+++ js/app.js
@@ -845,5 +845,5 @@
           var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
           dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
           (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-    }, 100);   
+    }, 1000);   
 })(window.jQuery);
