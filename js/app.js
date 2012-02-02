66853de31fd6d980897ea07349f7f62d77556440
Danny DeBelius <danny.debelius@gmail.com>
2012-02-02 14:22:11 -0500

    Really add disqus for IE

--- js/app.js
+++ js/app.js
@@ -843,7 +843,6 @@
     
     var disqus_developer = 1;
     var disqus_shortname = 'stateimpactid'; // required: replace example with your forum shortname
-    if (jQuery.browser.msie) return;
     var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
     dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
