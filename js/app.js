bc85aeb17f9023ca26928841f2103081034d00e4
Chris Amico <eyeseast@gmail.com>
2012-02-02 11:39:07 -0500

    No disqus for IE

--- js/app.js
+++ js/app.js
@@ -598,7 +598,7 @@
                             if (_.isString(this.value) && this.value.match(/\w+ \d{4}/)) {
                                 return this.value.replace("January", "");
                             } else {
-                                return ""
+                                return "";
                             }
                         },
                         x: 10,
@@ -841,6 +841,7 @@
     var disqus_developer = 1;
     var disqus_shortname = 'stateimpactid'; // required: replace example with your forum shortname
     setTimeout(function() {
+          if (jQuery.browser.msie) return;
           var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
           dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
           (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
