298ed7dc48aa7678104a2d6e8d02e3285382088e
Danny DeBelius <danny.debelius@gmail.com>
2012-02-03 09:48:11 -0500

    Fix webkit legend spacing bug

--- js/app.js
+++ js/app.js
@@ -848,3 +848,9 @@
     dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 })(window.jQuery);
+// Fixes webkit legend bug
+$.load(function () {
+    if (jQuery.browser.webkit){
+       hichart.chart.legend.renderLegend();
+    };
+});
